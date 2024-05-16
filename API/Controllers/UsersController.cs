955d8e07aa53d691fa1d401dada817e4ec2b7652
sholehch71 <sholehch71@gmail.com>
2024-05-15 21:12:58 -0400

    End of the section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : Controller
+    {
+        //We wnat to use the Context to the rest of the controller:
+        private readonly DataContext _context;
+
+        //How do we inject staff to the controller? with constructor
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("{id}")]  // /api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
