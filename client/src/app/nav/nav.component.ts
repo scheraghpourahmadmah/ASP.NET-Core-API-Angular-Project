706f2fd8ac554b799f1c13e1694bc36454f88b91
sholehch71 <sholehch71@gmail.com>
2024-06-05 15:24:35 -0400

    Communication from parent to child in both ways in Angular

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,22 +10,23 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit{
   model:any ={}
-  loggedIn = false;
 
-  constructor(private accountService: AccountService) {}
+  constructor(public accountService: AccountService) {}
+
   ngOnInit(): void {
   }
+  
   //observable
   login(){
     this.accountService.login(this.model).subscribe({
       next: response =>{
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   }
   logOut(){
-    this.loggedIn = false;
+    this.accountService.logout();
+    
   }
 }
